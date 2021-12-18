b0f9d1a6b5a09a1135fabc14a33755dae08c1a90
Hamed Rezaie Saniani <Hamed.r.Saniani@outlook.com>
2021-12-18 23:44:05 +0330

    Add project files.

---
+++ EvenOrOdd/Evaluator.cs
@@ -0,0 +1,10 @@
+﻿namespace EvenOrOdd
+{
+    public class Evaluator
+    {
+        public string Evaluate(int value)
+        {
+            return value % 2 == 0 ? "Even" : "Odd";
+        }
+    }
+}
---
+++ EvenOrOdd/EvaluatorTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace EvenOrOdd
+{
+    public class EvaluatorTests
+    {
+        [Fact]
+        public void Evaluate_Should_Return_Even()
+        {
+            const int number = 4;
+            var evaluator = new Evaluator();
+            var actual = evaluator.Evaluate(number);
+            Assert.Equal("Even",actual);
+        }
+        [Fact]
+        public void Evaluate_Should_Return_Odd()
+        {
+            const int number = 3;
+            var evaluator = new Evaluator();
+            var actual = evaluator.Evaluate(number);
+            Assert.Equal("Odd", actual);
+        }
+    }
+}
